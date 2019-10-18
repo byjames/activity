9f83ff76712b761989d75a4fdb758e2a5a112061
byjames <2105055520@qq.com>
2019-10-18 18:08:30 +0800

    Update define.php

--- lib/define.php
+++ lib/define.php
@@ -6,13 +6,13 @@ date_default_timezone_set(TIMEZONE);
 define('DBHOST', 'localhost');            // 数据库地址
 define('DBUSER', 'root');                 // 数据库用户名
 define('DBPORT', 3306);
-define('DBPASSWORD', 'lmj3503791673');    // 数据库密码
+define('DBPASSWORD', '保密
 define('DBNAME', 'gank_activites');              // 默认数据库
 define('DBCHARSET', 'utf8');              // 数据库字符集
 
 #wechat defaine
-define('WECHAT_APPID', 'wx6a7abf227bcd0d56');
-define('WECHAT_KEY', '13084de8fbb0038feeaa15c3d6b392e7');
+define('WECHAT_APPID', 'wx6a7abf227bcd0d');
+define('WECHAT_KEY', '13084de8fbb0038feeaa15c3d');
 define('WECHAT_SCOPE_BASE', 'snsapi_base'); // 不弹框
 define('WECHAT_SCOPE', 'snsapi_userinfo'); // 弹框
 
@@ -45,18 +45,18 @@ define('DATE_FORMAT_D', "Y-m-d");
 
 # res
 #file upload url
-//define('RES_URL', 'http://192.168.181.133:9095');
+//define('RES_URL', 'http://192.168.181.139:9095');
 //define('RES_PATH', dirname(dirname(dirname($_SERVER['DOCUMENT_ROOT']))) . '/res/config/');
-define('RES_CONFIG_URL', 'http://192.168.181.133/res/config');// config file url
+define('RES_CONFIG_URL', 'http://192.168.181.1303/res/config');// config file url
 define('RES_ATH', dirname(dirname(dirname($_SERVER['DOCUMENT_ROOT']))) . '/res/data/');
 define('LOCAL_DATA_PATH', $_SERVER['DOCUMENT_ROOT'] . '/res/data/');
 define('LOCAL_CFG_PATH', $_SERVER['DOCUMENT_ROOT'] . '/res/config/');
 #排行榜接口地址
 define('RANKING_FILE_DESC', '-'); // 文件链接符
 define('STAT_RANKING_DIR', 'http://event.djsh5.com/rank/?');    //  暂无用
-define('RES_LOAD_RANKING_URL', 'http://backup.djsh5.com/res/data/'); // get 排行数据脚本文件地址
+define('RES_LOAD_RANKING_URL', 'http://bacdjsh5.com/res/data/'); // get 排行数据脚本文件地址
 define('RES_LOAD_RANKING_PATH', $_SERVER['DOCUMENT_ROOT'] . '/res/data/'); //  文件的地址目录
-define('ACCEPT_FILE_SCRIPT_URL', 'http://192.168.181.133/res/ranking-load-file.php'); // 远程接收排行榜数据文件脚本url
+define('ACCEPT_FILE_SCRIPT_URL', 'http://192.168.181.137/res/ranking-load-file.php'); // 远程接收排行榜数据文件脚本url
 
 # 常数zero
 define('ZERO', 0);
